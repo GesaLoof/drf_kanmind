be7ddc686c043472df882225d98dee00a84a5ee4
GesaLoof <gesaloof@gmail.com>
2026-05-08 17:42:20 +0200

    formatted urls.py

--- board_app/api/urls.py
+++ board_app/api/urls.py
@@ -1,26 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (
-    BoardViewSet,
-    TestApiView,
-    EmailCheckView,
-    AssignedToMeView,
-    TaskViewSet,
-    ReviewingView,
-    CommentListCreateView,
-)
+from .views import BoardViewSet, TestApiView, EmailCheckView, AssignedToMeView, \
+    TaskViewSet, ReviewingView, CommentListCreateView
 
 router = DefaultRouter()
-router.register(r"boards", BoardViewSet, basename="board")
-router.register(r"tasks", TaskViewSet, basename="task")
+router.register(r'boards', BoardViewSet, basename='board')
+router.register(r'tasks', TaskViewSet, basename='task')
 
 urlpatterns = [
-    path("tasks/assigned-to-me/", AssignedToMeView.as_view(), name="assigned-to-me"),
-    path("tasks/reviewing/", ReviewingView.as_view(), name="reviewing"),
-    path(
-        "tasks/<int:pk>/comments/", CommentListCreateView.as_view()
-    ),  # Include comment URLs
-    path("", include(router.urls)),
-    path("test/", TestApiView.as_view(), name="api-test"),
-    path("email-check/", EmailCheckView.as_view(), name="email-check"),
+    path('tasks/assigned-to-me/', AssignedToMeView.as_view(), name='assigned-to-me'),
+    path('tasks/reviewing/', ReviewingView.as_view(), name='reviewing'),
+    path('tasks/<int:pk>/comments/', CommentListCreateView.as_view()),
+    path('', include(router.urls)),
+    path('test/', TestApiView.as_view(), name='api-test'),
+    path('email-check/', EmailCheckView.as_view(), name = 'email-check'), 
 ]
